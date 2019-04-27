32109e8e59d54927862bd7989ba111726fa3165e
Indra Lukmana <indralukmana@live.com>
2019-04-27 10:27:20 +0700

    Show excerpt on password protected post

--- wp-show-excerpt.php
+++ wp-show-excerpt.php
@@ -9,3 +9,22 @@ Author URI:  http://indralukmana.com
 License:     GPL2 etc
 License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
 */
+
+function wpb_protected_excerpt( $excerpt ) {
+    if ( post_password_required() ) {
+    $post = get_post();
+    $excerpt=$post->post_excerpt;
+    }
+    return $excerpt;
+}
+add_filter( 'the_excerpt', 'wpb_protected_excerpt' );
+
+function wpb_protected_excerpt_posts( $content ) {
+    if ( post_password_required() && is_single() ) {
+    $post = get_post();
+
+    return $post->post_excerpt.$content;
+    }
+    return $content;
+}
+add_filter( 'the_content', 'wpb_protected_excerpt_posts', 10 );
