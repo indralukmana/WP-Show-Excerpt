1be8664d6d99cd11c97df44e3a5f6e35d9791dfc
Indra Lukmana <indralukmana@live.com>
2019-04-27 10:06:11 +0700

    Init

---
+++ wp-show-excerpt.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WP Show Excerpt
+Plugin URI:  https://github.com/indralukmana/WP-Show-Excerpt
+Description: This plugin will show the excerpt of password protected content
+Version:     1.0
+Author:      Indra Lukmana
+Author URI:  http://indralukmana.com
+License:     GPL2 etc
+License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+*/
