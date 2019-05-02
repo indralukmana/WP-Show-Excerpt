5e0bc0505b123312c91272694f0ee66562210acb
Indra Lukmana <indralukmana@live.com>
2019-05-02 22:06:36 +0700

    Add function to hyperlink email in excerpt

--- wp-show-excerpt.php
+++ wp-show-excerpt.php
@@ -10,21 +10,31 @@ License:     GPL2 etc
 License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
 */
 
-function wpb_protected_excerpt( $excerpt ) {
+function wpse_protected_excerpt( $excerpt ) {
     if ( post_password_required() ) {
-    $post = get_post();
-    $excerpt=$post->post_excerpt;
+        $post = get_post();
+        $excerpt=$post->post_excerpt;
     }
+
+    $excerpt = change_email($excerpt);
+
     return $excerpt;
 }
-add_filter( 'the_excerpt', 'wpb_protected_excerpt' );
+add_filter( 'the_excerpt', 'wpse_protected_excerpt' );
 
-function wpb_protected_excerpt_posts( $content ) {
+function wpse_protected_excerpt_posts( $content ) {
     if ( post_password_required() && is_single() ) {
-    $post = get_post();
+        $post = get_post();
 
-    return $post->post_excerpt.$content;
+        return change_email($post->post_excerpt.$content);
     }
-    return $content;
+    return change_email($content);
 }
-add_filter( 'the_content', 'wpb_protected_excerpt_posts', 10 );
+add_filter( 'the_content', 'wpse_protected_excerpt_posts', 10 );
+
+function change_email($string) {
+    $search  = array('/<p>__<\/p>/', '/([a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4})/');
+    $replace = array('<hr />', '<a href="mailto:$1">$1</a>');
+    $processed_string = preg_replace($search, $replace, $string);
+    return $processed_string;
+}
